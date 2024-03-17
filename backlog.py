6101535f85712d3a4e6088b6fa3923c77a365903
root <root@job-896fb77a-6b8e-448e-9af5-883f21708130-87jqf>
2024-03-17 03:36:25 +0000

    Add docstrings to Backlog class

--- backlog.py
+++ backlog.py
@@ -1,18 +1,45 @@
 class Backlog:
+    """
+    A class to manage backlog items with functionalities to add, remove, list, and reprioritize items.
+    """
     def __init__(self):
+        """
+        Initializes the Backlog with an empty list of items.
+        """
         self.items = []
 
     def add_item(self, item):
+        """
+        Adds an item to the backlog.
+
+        :param item: The item to be added to the backlog.
+        """
         self.items.append(item)
 
     def remove_item(self, item):
+        """
+        Removes an item from the backlog if it exists.
+
+        :param item: The item to be removed from the backlog.
+        """
         if item in self.items:
             self.items.remove(item)
 
     def list_items(self):
+        """
+        Lists all items in the backlog.
+
+        :return: A list of all items in the backlog.
+        """
         return self.items
 
     def reprioritize_item(self, item, new_priority):
+        """
+        Reprioritizes an item in the backlog.
+
+        :param item: The item to be reprioritized.
+        :param new_priority: The new priority index for the item.
+        """
         if item in self.items:
             index = self.items.index(item)
             self.items.remove(item)

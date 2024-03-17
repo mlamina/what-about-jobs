21f41a7b7931c61d3a0770df2b7d2cd2bcee3908
root <root@job-74b8f4a7-6cee-4fd1-83f4-d5018d7579df-kpkkx>
2024-03-17 03:31:31 +0000

    Create Backlog class in backlog.py

---
+++ backlog.py
@@ -0,0 +1,19 @@
+class Backlog:
+    def __init__(self):
+        self.items = []
+
+    def add_item(self, item):
+        self.items.append(item)
+
+    def remove_item(self, item):
+        if item in self.items:
+            self.items.remove(item)
+
+    def list_items(self):
+        return self.items
+
+    def reprioritize_item(self, item, new_priority):
+        if item in self.items:
+            index = self.items.index(item)
+            self.items.remove(item)
+            self.items.insert(new_priority, item)
